9085fbbb795b810cc6d101bd26c30daf492a5bab
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-04-22 22:05:48 -0500

    improve belly class

--- src/belly.py
+++ src/belly.py
@@ -1,10 +1,8 @@
-# src/belly.py
-# from src.clock import get_current_time
-
 class Belly:
-    def __init__(self):
+    def __init__(self,modo_stress=False):
         self.pepinos_comidos = 0.0
         self.tiempo_esperado = 0.0
+        self.modo_stress = modo_stress 
 
     def reset(self):
         self.pepinos_comidos = 0
@@ -16,9 +14,8 @@ class Belly:
             raise ValueError("No se puede comer una cantidad negativa de pepinos")
         
         # Comprobar que no sobrepase la cantidad permitida
-        if pepinos > 100:
+        if not self.modo_stress and pepinos > 100:
             raise ValueError("No se puede comer más de 100 pepinos, es peligroso para la salud")
-        
         
         print(f"He comido {pepinos} pepinos.")
         self.pepinos_comidos += pepinos
